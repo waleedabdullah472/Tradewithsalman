595c5608c2588d9b663730f3fb141e2b1b785d45
waleedabdullah472 <waleedkhanii472@gmail.com>
2026-07-01 15:32:42 +0500

    fix: updated package json build scriptttt

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,3 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-  },
-  server: {
-    port: 5173,
-  }
-})
+export default defineConfig({ plugins: [react()] })
